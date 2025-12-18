0597e5952299cd46773be16d9f99d52e46555e67
DominicDev <s0l0estudi4r@gmail.com>
2025-12-18 13:32:13 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MON LAFERTE';
+export const nameSongs = [
+	"TU FALTA DE QUERER", /*1*/
+	"MI BUEN AMOR", /*2*/
+	"AMÁRRAME", /*3*/
+	"ANTES DE TI", /*4*/
+	"AMOR COMPLETO", /*5*/
+	"MI BUEN AMOR (EN VIVO)", /*6*/
+	"VUELVE POR FAVOR", /*7*/
+	"PA’ DÓNDE SE FUE", /*8*/
+	"SI TÚ ME QUISIERAS", /*9*/
+	"TORMENTO", /*10*/
+	"PRIMAVERAL", /*11*/
+	"EL BESO", /*12*/
+	"PLATA TA TÁ", /*13*/
+	"FUNERAL", /*14*/
+	"POR QUÉ ME FUI A ENAMORAR DE TI", /*15*/
+	"FLACO", /*16*/
+	"TU FALTA DE QUERER", /*17*/
+	"AMOR COMPLETO", /*18*/
+	"DEPRE", /*19*/
+	"FLOR DE AMAPOLA", /*20*/
+]
